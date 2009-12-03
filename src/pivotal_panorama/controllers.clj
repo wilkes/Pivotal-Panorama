7cc8b787a951052751fed274a4fbd8e9a5448b4a
wilkes <wilkesjoiner@gmail.com>
2009-12-03 09:27:02 -0600

    added index card like styling

--- src/pivotal_panorama/controllers.clj
+++ src/pivotal_panorama/controllers.clj
@@ -1,6 +1,7 @@
 (ns pivotal-panorama.controllers
   (:use [compojure :only [defroutes GET ANY serve-file
-                          html unordered-list link-to]]
+                          html unordered-list link-to
+                          include-css]]
         [clj-pt :only [user project projects current]]))
 
 (declare *pt-user*)
@@ -10,10 +11,9 @@
 (defn html-document [title & body]
   (html
    [:html
-    [:head [:title title]]
-    [:body
-     (unordered-list (map #(apply link-to %) [["/projects" "Projects"]]))
-     body]]))
+    [:head [:title title]
+     (include-css "/css/style.css")]
+    [:body body]]))
 
 (defn link-to-project [project]
   (link-to (str "/projects/" (-> project :project :id))
@@ -37,13 +37,14 @@
   (keyword (apply str strings)))
 
 (defn html-card [obj type card-class & keys]
-  (letfn [(make-content [k] [(tag "div." type "-" (name k)) (k obj)])]
-    [(tag "div." type "-card" (when card-class
+  (letfn [(make-content [k] [(tag "div.card-content." type "-" (name k)) (k obj)])]
+    [(tag "div.card." type "-card" (when card-class
                                 (str "." card-class)))
-     (map make-content keys)]))
+     (map make-content keys)
+     [:div.clear]]))
 
 (defn story-card [s]
-  (let [story (merge s {:url (link-to (:url s) "View in PivotalTracker")
+  (let [story (merge s {:url (link-to (:url s) "Edit")
                         :current_state (.replaceAll (:current_state s) "_" " ")})]
     (html-card story "story" (:current_state story)
                :name :description :owned_by :current_state :url)))
@@ -53,10 +54,10 @@
    "Current"
    (map (fn [[project iteration]]
           [:div.project
-           [:h3 (-> project :project :name)]
-           (unordered-list
-            (map #(story-card (:story %))
-                 (-> iteration first :iteration :stories)))])
+           [:h1 (-> project :project :name)]
+           (map #(story-card (:story %))
+                (-> iteration first :iteration :stories))
+           [:div.clear]])
         (map-projects current))))
 
 (defn not-implemented [feature-name]
@@ -66,11 +67,8 @@
      [:h1 title])))
 
 (defroutes app
-  (GET "/" (html-document "Pivotal Panorama" nil))
-  (GET "/current" (current-iterations))
+  (GET "/" (current-iterations))
   (GET "/projects" (list-projects request))
   (GET "/projects/:id" (not-implemented "Project Summary"))
-  (ANY "/*"(or (serve-file (params :*))
-               :next))
-  (ANY "*" [404 (str "<h1>404 - Not Found:" (:uri request) "</h1>")]))
-
+  (ANY "*"(or (serve-file (params :*))
+              [404 (str "<h1>404 - Not Found:" (:uri request) "</h1>")])))
