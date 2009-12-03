4b32e45b6acae247b5ed8500f9eec6c5440681de
wilkes <wilkesjoiner@gmail.com>
2009-12-03 14:05:07 -0600

    updated to clj-pt 0.1.1

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
 		 [org.clojars.ato/compojure "0.3.1"]
-                 [clj-pt "0.1.0"]]
+                 [clj-pt "0.1.1"]]
   :dev-dependencies [[swank-clojure "1.1.0-SNAPSHOT"]]
   :main pivotal_panorama)
