7be2aba8176026504dd92887fbe1cad083b26fec
wilkes <wilkesjoiner@gmail.com>
2009-12-16 17:31:23 -0600

    refactored filter-results

--- src/pivotal_panorama/dispatch.clj
+++ src/pivotal_panorama/dispatch.clj
@@ -2,7 +2,6 @@
   (:use [compojure :only [defroutes GET ANY serve-file redirect-to]]
         [clj-pt :only [user project projects current]]
         [clojure.contrib.seq-utils :only [flatten]]
-        [clojure.contrib.str-utils2 :only [upper-case blank?]]
         [pivotal-panorama.html :only [current-by]]
         clojure.contrib.pprint)
   (:import [java.io File]))
@@ -10,7 +9,6 @@
 (declare *pt-user*)
 (defn set-api-token [token]
   (def *pt-user* (user token)))
-
 
 (defn on-project [p args]
   (future [p (apply (*pt-user* project (-> p :id)) args)]))
@@ -22,29 +20,24 @@
   (apply merge-with concat
          (map (fn [v] {(index-fn v) [v]}) ms)))
 
-(defn group-iteration [group-by p iteration]
-  (let [stories (map #(merge % {:project-link (str "http://www.pivotaltracker.com/projects/" (p :id))
-                                :project-name (:name p)})
-                     (:stories iteration))]
+(defn group-iteration [group-by story-filter p iteration]
+  (let [story-filter #(if story-filter
+                        (= (:current_state %) story-filter)
+                        true)
+        add-project-info #(merge % {:project-link (str "http://www.pivotaltracker.com/projects/" (p :id))
+                                    :project-name (:name p)})
+        stories (map add-project-info (filter story-filter (:stories iteration)))]
     (if (= group-by :project)
       {(:name p) stories}
       (index-maps stories group-by))))
 
-(defn fetch-stories [iteration-fn group-by]
+(defn fetch-stories [iteration-fn group-by story-filter]
   (let [group-stories (fn [[p iterations]]
-                          (map #(group-iteration group-by p %) iterations))]
+                          (map #(group-iteration group-by story-filter p %) iterations))]
     (apply merge-with concat
            (flatten (map group-stories (map-projects iteration-fn))))))
 
-(defn filter-by-story [story-state rs]
-  (apply merge (map #(hash-map (first %) (filter (fn [s]  (= (upper-case (s :current_state))
-                                                        (upper-case story-state)))
-                             (second %))) rs)))
-
-(defn filter-results [rs story-filter] (if-not
-                                     (blank? story-filter)
-                                   (filter-by-story story-filter rs)
-                                   rs)) 
+ 
 
 (defn serve-classpath-file
   "Serves a file off the classpath, i.e. bundled in the jar."
@@ -67,9 +60,8 @@
          (current-by iteration
                      group-by
                      story-filter
-                     (filter-results (fetch-stories iterfn (keyword group-by)) story-filter))))
+                     (fetch-stories iterfn (keyword group-by) story-filter))))
   (ANY "*"
        (or (serve-file (params :*))
            (serve-classpath-file (params :*))
-           [404 (str "<h1>404 - Not Found:" (:uri request) "</h1>")]))
-)
+           [404 (str "<h1>404 - Not Found:" (:uri request) "</h1>")])))
