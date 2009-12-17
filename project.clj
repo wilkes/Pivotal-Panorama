27e50da81bb56ac0699d8edcbe5c9ab3a0f84c8f
wilkes <wilkesjoiner@gmail.com>
2009-12-16 21:14:30 -0600

    version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pivotal-panorama "0.1.0-SNAPSHOT"
+(defproject pivotal-panorama "0.1.0"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
 		 [org.clojars.ato/compojure "0.3.1"]
